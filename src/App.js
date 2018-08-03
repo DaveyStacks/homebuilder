81616588fbeeeaed896e11ce95d1df987e0ef66c
David Coulombe <David.Coulombe7@gmail.com>
2018-08-03 09:53:36 -0400

    app with routes basic

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Route } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,13 +7,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="main">
+          <Route exact path="/" component={Home} />
+          <Route path="/homemodel" />
+          <Route path="/homemodel/:caption" />
+
+          <Route path="/guest" component={Guest} />
+        </div>
       </div>
     );
   }
